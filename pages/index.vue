d2760141109e625aeccedb9f8e0e77490587b17c
Konosuke Takeshima <k.takeshima@degiple.com>
2023-06-24 23:43:52 +0900

    add-quasar

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,5 @@
 <template>
-  <div>
-    <form @submit.prevent="($event) => createTask(newTask)">
-      <input v-model="newTask" autocomplete="off" />
-      <button>Add</button>
-    </form>
-    <ul>
-      <li v-for="task in tasks">
-        {{ task.id }}
-        <input v-model="task.name" type="text" />
-        <input v-model="task.isComplete" type="checkbox" />
-        <button @click="updateTask(task)">Update</button>
-        <button @click="deleteTask(task.id)">Delete</button>
-      </li>
-    </ul>
-    <button @click="clearTask">Clear</button>
-  </div>
+  <q-btn color="primary" label="Primary" />
+  <QBtn color="secondary" label="Secondary" />
+  <LazyQBtn color="amber" glossy label="Amber" />
 </template>
-
-<script setup lang="ts">
-const { tasks, createTask, updateTask, deleteTask, clearTask } = useTasksState()
-// const { tasks, createTask, updateTask, deleteTask, clearTask } = useTaskCookie()
-
-const newTask = ref('')
-</script>
